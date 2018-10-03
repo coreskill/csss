c6f3905c452eba7397f481c8ee470abcfa449bdc
Kate Miháliková <kate@katemihalikova.cz>
2018-10-03 15:17:41 +0200

    Add third indicator state

--- src/firebase.js
+++ src/firebase.js
@@ -30,15 +30,18 @@ function addFirebaseListener() {
 let indicators = document.querySelectorAll(".indicator");
 function setStateOfAllIndicators(data) {
   indicators.forEach(indicator => {
-    indicator.classList.toggle("is-done", Boolean(data[indicator.dataset.slug]));
+    indicator.classList.toggle("is-done", data[indicator.dataset.slug] === true);
+    indicator.classList.toggle("is-star", data[indicator.dataset.slug] === 'star');
   });
 }
 
 function indicatorListener(event) {
   let isDone = this.classList.contains("is-done");
+  let isStar = this.classList.contains("is-star");
+  let newValue = isDone ? false : isStar ? true : 'star';
   firebase.firestore().collection("db").doc("v1")
     .collection("users").doc(sessionStorage.getItem(SELECTED_USER_ID))
-    .set({[this.dataset.slug]: !isDone}, {merge: true})
+    .set({[this.dataset.slug]: newValue}, {merge: true})
     .catch(error => console.error(error));
   event.preventDefault();
 };
