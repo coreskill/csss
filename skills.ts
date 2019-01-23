bae6bad10b229f6dd69b7dff9fec25b83ed0e25b
Kate Miháliková <kate@katemihalikova.cz>
2019-01-23 14:53:24 +0100

    Add also index and graph to special slugs

--- skills.ts
+++ skills.ts
@@ -36,7 +36,7 @@ export interface Skill extends CNode {
   sameLevelSkills: Skill[];
 }
 
-const slugs = new Set(['constructor']);
+const slugs = new Set(['index', 'graph', 'constructor']);
 const getSlug = (input: string) => {
   let slugFromInput = slug(input).toLowerCase();
   let finalSlug = slugFromInput;
