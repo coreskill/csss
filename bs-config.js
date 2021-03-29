e2f586412c62a17e8f12374dce701cbdd7b3e6cf
DanielSrb <srb.dan@gmail.com>
2021-03-29 22:36:29 +0200

    working dev script (watching only one file in build folder)

---
+++ bs-config.js
@@ -0,0 +1,87 @@
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+  "ui": false,
+  "files": "build/index.html",
+  "watchEvents": [
+    "change"
+  ],
+  "watch": false,
+  "ignore": [],
+  "single": false,
+  "watchOptions": {
+    "ignoreInitial": true
+  },
+  "server": "build",
+  "proxy": false,
+  "port": 7777,
+  "middleware": false,
+  "serveStatic": [],
+  "ghostMode": false,
+  "logLevel": "info",
+  "logPrefix": "Browsersync",
+  "logConnections": false,
+  "logFileChanges": true,
+  "logSnippet": true,
+  "rewriteRules": [],
+  "open": "local",
+  "browser": "default",
+  "cors": false,
+  "xip": false,
+  "hostnameSuffix": false,
+  "reloadOnRestart": false,
+  "notify": true,
+  "scrollProportionally": true,
+  "scrollThrottle": 0,
+  "scrollRestoreTechnique": "window.name",
+  "scrollElements": [],
+  "scrollElementMapping": [],
+  "reloadDelay": 0,
+  "reloadDebounce": 500,
+  "reloadThrottle": 500,
+  "plugins": [],
+  "injectChanges": false,
+  "startPath": null,
+  "minify": true,
+  "host": null,
+  "localOnly": true,
+  "codeSync": true,
+  "timestamps": false,
+  "socket": {
+    "socketIoOptions": {
+      "log": false
+    },
+    "socketIoClientConfig": {
+      "reconnectionAttempts": 50
+    },
+    "path": "/browser-sync/socket.io",
+    "clientPath": "/browser-sync",
+    "namespace": "/browser-sync",
+    "clients": {
+      "heartbeatTimeout": 5000
+    }
+  },
+  "tagNames": {
+    "less": "link",
+    "scss": "link",
+    "css": "link",
+    "jpg": "img",
+    "jpeg": "img",
+    "png": "img",
+    "svg": "img",
+    "gif": "img",
+    "js": "script"
+  },
+  "injectNotification": false
+};
